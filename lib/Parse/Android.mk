6934d0b93443f49fe461c6bff12bb2c80a161c1b
Paul Beeler <sparksco@gmail.com>
2013-05-15 18:10:07 -0400

    clang: Fix build with gcc 4.8+
    Change-Id: Id965fa33cb1d8481ac778f5ec890b6d865aeb710

--- lib/Parse/Android.mk
+++ lib/Parse/Android.mk
@@ -32,7 +32,8 @@ clang_parse_SRC_FILES :=  \
 	Parser.cpp
 
 LOCAL_SRC_FILES := $(clang_parse_SRC_FILES)
-LOCAL_CFLAGS := -fno-strict-aliasing
+LOCAL_CFLAGS := -fno-strict-aliasing $(call-cc-option,-Qunused-arguments)
+LOCAL_CPPFLAGS := -fno-strict-aliasing
 
 LOCAL_MODULE:= libclangParse
 LOCAL_MODULE_TAGS := optional
